f87d4581c0c8d79b7e032882b639b727b1e341e8
xiaoyong.chen <jiahui.work9527@gmail.com>
2022-01-17 14:37:09 +0800

    fix: 配置全局默认样式normalize.css

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// 全局变量
 import "amfe-flexible";
+import "normalize.css/normalize.css";
 
 createApp(App).use(store).use(router).mount("#app");
